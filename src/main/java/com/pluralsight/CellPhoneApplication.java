5767e8707d8c01966e5ca3fe58d6e44f534a3f1e
YairVera <yairvera44@gmail.com>
2025-10-02 09:17:47 -0500

    user input scanner

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,15 +1,37 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         CellPhone phone1 = new CellPhone();
-        phone1.setSerialNumber(1234567_1234567L);
-        phone1.setCarrier("T-Mobil");
-        phone1.setModel("iPhone 14 Pro");
-        phone1.setPhoneNumber("(773)323-2323");
-        phone1.setOwner("John Cena");
+        System.out.print("Serial number: ");
+
+        phone1.setSerialNumber(12345677654321L);
+        phone1.setCarrier("Carrier: T-Mobile");
+        phone1.setModel("Model: iPhone 14 Pro");
+        phone1.setPhoneNumber("Phone number: (800)588-2300");
+        phone1.setOwner("Owner: John Cena");
         phone1.display();
+
+        System.out.println("========== User Input ==========");
+
+        CellPhone phone2 = new CellPhone();
+        System.out.print("What is the serial number? ");
+        phone2.setSerialNumber(scanner.nextLong());
+        scanner.nextLine();
+        System.out.print("Who is your carrier? ");
+        phone2.setCarrier(scanner.nextLine());
+        System.out.print("What model is your phone? ");
+        phone2.setModel(scanner.nextLine());
+        System.out.print("What is the phone number? ");
+        phone2.setPhoneNumber(scanner.nextLine());
+        System.out.print("Who is the owner? ");
+        phone2.setOwner(scanner.nextLine());
+
+
     }
 
 }
